c35494e2fc1d489265fcfa3fb0b2fc72e933a682
Adam Yeager <adam.yeager@gmail.com>
2015-10-14 20:00:47 -0400

    Updated the AssemblyInfo file.
    Signed-off-by: Adam Yeager <adam.yeager@gmail.com>

--- PushbulletSharp/Properties/AssemblyInfo.cs
+++ PushbulletSharp/Properties/AssemblyInfo.cs
@@ -7,9 +7,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("PushbulletSharp")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyDescription("A simple C# wrapper to PushBullet's HTTP API.")]
+[assembly: AssemblyConfiguration("Release")]
+[assembly: AssemblyCompany("Adam Yeager")]
 [assembly: AssemblyProduct("PushbulletSharp")]
 [assembly: AssemblyCopyright("Copyright ©  2015 Adam Yeager")]
 [assembly: AssemblyTrademark("")]
