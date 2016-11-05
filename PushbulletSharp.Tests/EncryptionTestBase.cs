a116c781015d5623c19264744e2d09b5b4d55155
Adam Yeager <adam.yeager@gmail.com>
2016-11-05 13:56:00 -0400

    Added a new EncriptionBase class for testing encrypted messaging. Also added two test for testing PushBullet's style of encription.
    Signed-off-by: Adam Yeager <adam.yeager@gmail.com>

---
+++ PushbulletSharp.Tests/EncryptionTestBase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PushbulletSharp.Tests
+{
+    public abstract class EncryptionTestBase
+    {
+        public PushbulletClient Client { get; set; }
+        public string ApiKey { get; set; }
+
+        [TestInitialize]
+        public void TestInit()
+        {
+            ApiKey = "--YOURKEYGOESHERE--";
+            Client = new PushbulletClient(ApiKey, "--YOUR-ENCRYPTION-PASSWORD--", TimeZoneInfo.Local);
+
+            //Optional pass in your timezone
+            //Client = new PushbulletClient(ApiKey, TimeZoneInfo.Local);
+
+            //Or pass in a specific timezone
+            //Client = new PushbulletClient(ApiKey, "password", TimeZoneInfo.FindSystemTimeZoneById("Eastern Standard Time"));
+        }
+    }
+}
---
+++ PushbulletSharp.Tests/EncryptionTests.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace PushbulletSharp.Tests
+{
+    [TestClass]
+    public class EncryptionTests : EncryptionTestBase
+    {
+        /// <summary>
+        /// Encryptions the test.
+        /// </summary>
+        [TestMethod]
+        public void EncryptionTest()
+        {
+            try
+            {
+                // The provided test key is from PushBullet's API doc
+                string message = "This is a test from the PushBulletSharp library!";
+                string key = "1sW28zp7CWv5TtGjlQpDHHG4Cbr9v36fG5o4f74LsKg=";
+                string encryptedMessage = Encryption.EncryptionUtility.EncryptMessage(message, key);
+
+                Assert.IsNotNull(encryptedMessage);
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Decryptions the test.
+        /// </summary>
+        [TestMethod]
+        public void DecryptionTest()
+        {
+            try
+            {
+                // The provided test message & key are from PushBullet's API doc (it should say "meow!")
+                string encryptedMessage = "MSfJxxY5YdjttlfUkCaKA57qU9SuCN8+ZhYg/xieI+lDnQ==";
+                string key = "1sW28zp7CWv5TtGjlQpDHHG4Cbr9v36fG5o4f74LsKg=";
+                string message = Encryption.EncryptionUtility.DecryptMessage(encryptedMessage, key);
+
+                Assert.IsTrue(message == "meow!");
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail(ex.Message);
+            }
+        }
+    }
+}
