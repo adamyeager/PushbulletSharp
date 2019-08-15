f18f59cee069911a2c361528f24f2af0da0578c8
Jonas Van der Aa <ketsukonn@gmail.com>
2019-08-15 22:20:28 +0200

    Switch to AesEngine
    AesFastEngine will be deprecated

--- PushbulletSharp/Encryption/EncryptionUtility.cs
+++ PushbulletSharp/Encryption/EncryptionUtility.cs
@@ -67,7 +67,7 @@ namespace PushbulletSharp.Encryption
             var nonce = new byte[NonceBitSize / 8];
             Random.NextBytes(nonce, 0, nonce.Length);
 
-            var cipher = new GcmBlockCipher(new AesFastEngine());
+            var cipher = new GcmBlockCipher(new AesEngine());
             var parameters = new AeadParameters(new KeyParameter(keyBytes), TagBitSize, nonce);
             cipher.Init(true, parameters);
 
@@ -142,7 +142,7 @@ namespace PushbulletSharp.Encryption
                     // Get the Nonce (IV)
                     var nonce = cipherReader.ReadBytes(NonceBitSize / 8);
 
-                    var cipher = new GcmBlockCipher(new AesFastEngine());
+                    var cipher = new GcmBlockCipher(new AesEngine());
                     var parameters = new AeadParameters(new KeyParameter(keyBytes), TagBitSize, nonce);
                     cipher.Init(false, parameters);
 
